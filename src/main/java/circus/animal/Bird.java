f42d4a062a361157690b8e353f26820d9460b6d6
Li Bozhao <libozhao142857@hotmail.com>
2020-09-04 17:25:12 +0800

    Added changes in lecture

--- src/main/java/circus/animal/Bird.java
+++ src/main/java/circus/animal/Bird.java
@@ -1,4 +1,9 @@
 public class Bird extends Animal {
+    @Override
+    public int getValue() {
+        return 30;
+    }
+
     public void fly() {
         System.out.println("Whee ...");
     }
--- src/main/java/circus/animal/Duck.java
+++ src/main/java/circus/animal/Duck.java
@@ -9,6 +9,11 @@ public class Duck extends Bird {
         return "I'm a Duck";
     }
 
+    @Override
+    public int getValue() {
+        return 10;
+    }
+
     public void swim() {
         System.out.println("I'm swimming...");
     }
